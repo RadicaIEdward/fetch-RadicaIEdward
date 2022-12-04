8e9144c24e6f1c0f7139d2bb0afde255d67a0975
RadicaIEdward <edward.o.galindez@gmail.com>
2022-12-04 15:16:47 -0500

    Add error handling

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// eslint-disable no-console
 // fetch('demo.json')
 //   .then((res) => {
 //     console.log(res);
@@ -16,13 +17,14 @@
 const postSection = document.querySelector('#posts');
 const postTemplate = document.querySelector('#post-template');
 
-getData();
+getData().catch((err) => console.error(err));
 
 async function getData() {
   const postStream = await fetch('https://jsonplaceholder.typicode.com/posts');
   const posts = await postStream.json();
   let i = 0;
 
+  // throw 'Get Data Error';
   // console.log(posts);
 
   posts.forEach((post) => {
@@ -39,11 +41,14 @@ async function getData() {
           const postBody = newPost.querySelector('.post__body');
           const postImg = newPost.querySelector('.post__img');
 
+          // throw 'Image Fetch Error';
+
           postImg.src = URL.createObjectURL(blob);
           postTitle.innerText = title;
           postBody.innerText = body;
           postSection.appendChild(newPost);
-        });
+        })
+        .catch((err) => console.error(err));
     }
   });
 }
