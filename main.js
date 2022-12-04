876d601f1b6ea1f3be2014017b49a7e70f9e7f82
RadicaIEdward <edward.o.galindez@gmail.com>
2022-12-04 14:32:10 -0500

    Complete fetch image function

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-fetch('demo.json')
-  .then((res) => {
-    console.log(res);
-    return res.json();
-  })
-  .then((json) => console.log(json));
+// fetch('demo.json')
+//   .then((res) => {
+//     console.log(res);
+//     return res.json();
+//   })
+//   .then((json) => console.log(json));
+
+fetch('http://unsplash.it/600/400')
+  .then((res) => res.blob())
+  .then((blob) => {
+    let img = document.createElement('img');
+    img.src = URL.createObjectURL(blob);
+    document.querySelector('body').appendChild(img);
+  });
